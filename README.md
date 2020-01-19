5493f618ea34d319dd58e395df2700dfcee43f71
Crayonn <wecdovey@gmail.com>
2020-01-19 09:44:01 +0800

    mod: readme.md

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 ### 使用方式
 
 ```js
+npm install // 下载依赖模块
 npm start // 开发
 
 npm run start:prod // 启动生产mock服务
 ```
 
-ps： config 为参数配置（mock文件路径、端口等）
+ps： config 为参数配置(mock文件路径、mock文件类型、ip、端口);
--- config/index.js
+++ config/index.js
@@ -1,7 +1,6 @@
-const path = require('path');
 module.exports = {
     mockPath: './mock', // mock文件存放目录
     extensions: /.js$/, // mock服务注入的文件类型
     HOST: 'localhost', // 启动服务host
     POST: 3007 // 端口
-}
+};
